4029aeb86cc39ac0993be3a2de341079f78e7e72
Will <will@localhost.localdomain>
2018-11-23 22:05:45 +0000

    This commit adds a scraper that can pull relevant html from bbcgood food and store it into a mongo database.

---
+++ goodfood/goodfood/spiders/scraper.py
@@ -0,0 +1,28 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from goodfood.items import RecipeItem
+
+class scraper(CrawlSpider):
+    name = 'scraper'
+    allowed_domains = ['bbcgoodfood.com']
+    start_urls = ['https://www.bbcgoodfood.com/recipes/764647/sausage-and-bean-onepot']
+    count = 0
+    rules = (
+        Rule(LinkExtractor(allow=(r'https://www.bbcgoodfood.com/recipes/\d*/.*$')), callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+        self.logger.info('Hi, this is an item page! %s', response.url)
+        item = RecipeItem()
+        item['ingredients'] = response.xpath(r'//*[@id="recipe-ingredients"]//li').extract()#.re_first(r'content="([\w\s]*)">')
+        item['method'] = response.xpath(r'//*[@id="recipe-method"]/div/ol').extract()
+        item['nutrition'] = response.xpath(r'//*[@id="recipe-header-more-info"]/div/div[2]/ul').extract()
+        item['title'] = response.css(".recipe-header__title").extract()
+        item['rating'] = response.css(".recipe-header__rating").extract()
+        item['prep_time'] = response.css(".recipe-details__cooking-time-prep").extract()
+        item['cook_time'] = response.css(".recipe-details__cooking-time-cook").extract()
+        item['skill'] = response.css(".recipe-details__item--skill-level").extract()
+        item['servings'] = response.css(".recipe-details__item--servings").extract()
+        print(item['title'])
+        return item
