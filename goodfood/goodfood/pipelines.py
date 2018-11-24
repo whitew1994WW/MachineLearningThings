a602a693af0aa360deedb33cdf79cba71a734f61
Will <will@localhost.localdomain>
2018-11-24 13:51:07 +0000

    Now stores raw html in the mongo database

--- goodfood/goodfood/pipelines.py
+++ goodfood/goodfood/pipelines.py
@@ -9,6 +9,7 @@ import pymongo
 from scrapy.conf import settings
 from scrapy.exceptions import DropItem
 from scrapy import log
+import re
 
 class MongoPipeline(object):
     collection_name = 'scrapy_items'
@@ -25,6 +26,8 @@ class MongoPipeline(object):
         self.collection = db[settings['MONGODB_COLLECTION']]
 
     def process_item(self, item, spider):
+        item = extract_info(item)
+
         valid = True
         for data in item:
             if not data:
@@ -35,3 +38,53 @@ class MongoPipeline(object):
             log.msg("Question added to MongoDB database!",
                     level=log.DEBUG, spider=spider)
         return item
+
+    def extract_info(self, item):
+        # Create a new object to reieve the actual values
+        item_extracted = {}
+        item_extracted['ingredients'] = extract_ingredients(item)
+        item_extracted['method'] = extract_method(item)
+        item_extracted['nutrition'] = extract_nutrition(item)
+        item_extracted['title'] = extract_title(item)
+        item_extracted['rating'] = extract_rating(item)
+        item_extracted['prep_time'] = extract_prep_time(item)
+        item_extracted['cook_time'] = extract_cook_time(item)
+        item_extracted['skill'] = extract_skill(item)
+        item_extracted['servings'] = extract_servings(item)
+        return item_extracted
+
+    def extract_ingredients(self, item):
+
+        return ingredients
+
+    def extract_method(self, item):
+
+        return method
+
+    def extract_nutrition(self, item):
+
+        return nutrition
+
+    def extract_title(self, item):
+
+        return title
+
+    def extract_rating(self, item):
+
+        return rating
+
+    def extract_prep_time(self, item):
+
+        return prep_time
+
+    def extract_cook_time(self, item):
+
+        return cook_time
+
+    def extract_skill(self, item):
+
+        return skill
+
+    def extract_servings(self, item):
+
+        return servings
